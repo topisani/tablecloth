13dd852e4a5557043c13a805d3f63facbea1f2e6
topisani <topisani@hamsterpoison.com>
2018-08-02 19:02:18 +0200

    gtk with layer-shell, C implementation

--- cloth-bar/test.cpp
+++ cloth-bar/test.cpp
@@ -6,6 +6,8 @@
 #endif
 #include <GLES2/gl2.h>
 #include <assert.h>
+#include <gdk/gdkwayland.h>
+#include <gtk/gtk.h>
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,112 +15,25 @@
 #include <time.h>
 #include <unistd.h>
 #include <wayland-client.h>
-#include <wayland-egl.h>
+
 #define namespace namespace_
 extern "C" {
-  #include "wlr-layer-shell-unstable-v1-client-protocol.h"
-  #include <wlr/render/egl.h>
-  #include <wlr/util/log.h>
-  #include "xdg-shell-client-protocol.h"
+#include <wlr/util/log.h>
+#include "wlr-layer-shell-unstable-v1-client-protocol.h"
 }
 #undef namespace
 
 static struct wl_display* display;
-static struct wl_compositor* compositor;
-static struct wl_shm* shm;
-static struct xdg_wm_base* xdg_wm_base;
 static struct zwlr_layer_shell_v1* layer_shell;
 
 struct zwlr_layer_surface_v1* layer_surface;
 struct wl_surface* wl_surface;
-struct wlr_egl egl;
-struct wl_egl_window* egl_window;
-struct wlr_egl_surface* egl_surface;
 struct wl_callback* frame_callback;
 
 static uint32_t layer = ZWLR_LAYER_SHELL_V1_LAYER_TOP;
-static uint32_t anchor = 0;
-static uint32_t width = 256, height = 256;
-static int32_t margin_top = 0;
-static double alpha = 1.0;
 static bool run_display = true;
-static bool animate = false;
-static bool keyboard_interactive = false;
-static double frame = 0;
-static int cur_x = -1, cur_y = -1;
-static int buttons = 0;
 
-static struct {
-  struct timespec last_frame;
-  float color[3];
-  int dec;
-} demo;
-
-static void draw(void);
-
-static void surface_frame_callback(void* data, struct wl_callback* cb, uint32_t time)
-{
-  wl_callback_destroy(cb);
-  frame_callback = NULL;
-  draw();
-}
-
-static struct wl_callback_listener frame_listener = {.done = surface_frame_callback};
-
-static void draw(void)
-{
-  eglMakeCurrent(egl.display, egl_surface, egl_surface, egl.context);
-  struct timespec ts;
-  clock_gettime(CLOCK_MONOTONIC, &ts);
-
-  long ms =
-    (ts.tv_sec - demo.last_frame.tv_sec) * 1000 + (ts.tv_nsec - demo.last_frame.tv_nsec) / 1000000;
-  int inc = (demo.dec + 1) % 3;
-
-  if (!buttons) {
-    demo.color[inc] += ms / 2000.0f;
-    demo.color[demo.dec] -= ms / 2000.0f;
-
-    if (demo.color[demo.dec] < 0.0f) {
-      demo.color[inc] = 1.0f;
-      demo.color[demo.dec] = 0.0f;
-      demo.dec = inc;
-    }
-  }
-
-  if (animate) {
-    frame += ms / 50.0;
-    int32_t old_top = margin_top;
-    margin_top = -(20 - ((int) frame % 20));
-    if (old_top != margin_top) {
-      zwlr_layer_surface_v1_set_margin(layer_surface, margin_top, 0, 0, 0);
-      wl_surface_commit(wl_surface);
-    }
-  }
-
-  glViewport(0, 0, width, height);
-  if (buttons) {
-    glClearColor(1, 1, 1, alpha);
-  } else {
-    glClearColor(demo.color[0], demo.color[1], demo.color[2], alpha);
-  }
-  glClear(GL_COLOR_BUFFER_BIT);
-
-  if (cur_x != -1 && cur_y != -1) {
-    glEnable(GL_SCISSOR_TEST);
-    glScissor(cur_x, height - cur_y, 5, 5);
-    glClearColor(0, 0, 0, 1);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glDisable(GL_SCISSOR_TEST);
-  }
-
-  frame_callback = wl_surface_frame(wl_surface);
-  wl_callback_add_listener(frame_callback, &frame_listener, NULL);
-
-  eglSwapBuffers(egl.display, egl_surface);
-
-  demo.last_frame = ts;
-}
+GtkWidget* window;
 
 static void layer_surface_configure(void* data,
                                     struct zwlr_layer_surface_v1* surface,
@@ -126,18 +41,15 @@ static void layer_surface_configure(void* data,
                                     uint32_t w,
                                     uint32_t h)
 {
-  width = w;
-  height = h;
-  if (egl_window) {
-    wl_egl_window_resize(egl_window, width, height, 0, 0);
+  if (window) {
+    gtk_window_resize(GTK_WINDOW(window), w, h);
   }
   zwlr_layer_surface_v1_ack_configure(surface, serial);
 }
 
 static void layer_surface_closed(void* data, struct zwlr_layer_surface_v1* surface)
 {
-  wlr_egl_destroy_surface(&egl, egl_surface);
-  wl_egl_window_destroy(egl_window);
+  gtk_window_close(GTK_WINDOW(window));
   zwlr_layer_surface_v1_destroy(surface);
   wl_surface_destroy(wl_surface);
   run_display = false;
@@ -154,14 +66,9 @@ static void handle_global(void* data,
                           const char* interface,
                           uint32_t version)
 {
-  if (strcmp(interface, wl_compositor_interface.name) == 0) {
-    compositor = (wl_compositor*) wl_registry_bind(registry, name, &wl_compositor_interface, 1);
-  } else if (strcmp(interface, wl_shm_interface.name) == 0) {
-    shm = (wl_shm*) wl_registry_bind(registry, name, &wl_shm_interface, 1);
-  } else if (strcmp(interface, zwlr_layer_shell_v1_interface.name) == 0) {
-    layer_shell = (zwlr_layer_shell_v1*) wl_registry_bind(registry, name, &zwlr_layer_shell_v1_interface, 1);
-  } else if (strcmp(interface, xdg_wm_base_interface.name) == 0) {
-    xdg_wm_base = (struct xdg_wm_base*) wl_registry_bind(registry, name, &xdg_wm_base_interface, 1);
+  if (strcmp(interface, zwlr_layer_shell_v1_interface.name) == 0) {
+    layer_shell =
+      (zwlr_layer_shell_v1*) wl_registry_bind(registry, name, &zwlr_layer_shell_v1_interface, 1);
   }
 }
 
@@ -179,10 +86,12 @@ int main(int argc, char** argv)
 {
   wlr_log_init(WLR_DEBUG, NULL);
   const char* _namespace = "wlroots";
-  int exclusive_zone = 0;
-  int32_t margin_right = 0, margin_bottom = 0, margin_left = 0;
 
-  display = wl_display_connect(NULL);
+  gtk_init(&argc, &argv);
+
+  GdkDisplay* gdk_display = gdk_display_get_default();
+
+  display = gdk_wayland_display_get_wl_display(gdk_display);
   if (display == NULL) {
     fprintf(stderr, "Failed to create display\n");
     return 1;
@@ -192,45 +101,42 @@ int main(int argc, char** argv)
   wl_registry_add_listener(registry, &registry_listener, NULL);
   wl_display_roundtrip(display);
 
-  if (compositor == NULL) {
-    fprintf(stderr, "wl_compositor not available\n");
-    return 1;
-  }
-  if (shm == NULL) {
-    fprintf(stderr, "wl_shm not available\n");
-    return 1;
-  }
-  if (layer_shell == NULL) {
-    fprintf(stderr, "layer_shell not available\n");
-    return 1;
-  }
+  GtkWidget *box1, *button;
 
-  EGLint attribs[] = {EGL_ALPHA_SIZE, 8, EGL_NONE};
-  wlr_egl_init(&egl, EGL_PLATFORM_WAYLAND_EXT, display, attribs, WL_SHM_FORMAT_ARGB8888);
+  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 
-  wl_surface = wl_compositor_create_surface(compositor);
+  gtk_window_set_title(GTK_WINDOW(window), "gtk shell");
+  gtk_window_set_decorated(GTK_WINDOW(window), FALSE);
+  gtk_widget_realize(window);
+
+  box1 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+  gtk_container_add(GTK_CONTAINER(window), box1);
+
+  button = gtk_button_new_with_label("launch terminal");
+  gtk_box_pack_start(GTK_BOX(box1), button, TRUE, TRUE, 0);
+  gtk_widget_show(button);
+
+  gtk_widget_show(box1);
+
+  GdkWindow* gdk_window = gtk_widget_get_window(window);
+
+  gdk_wayland_window_set_use_custom_surface(gdk_window);
+
+  wl_surface = gdk_wayland_window_get_wl_surface(gdk_window);
   assert(wl_surface);
 
   layer_surface =
     zwlr_layer_shell_v1_get_layer_surface(layer_shell, wl_surface, nullptr, layer, _namespace);
   assert(layer_surface);
-  zwlr_layer_surface_v1_set_size(layer_surface, width, height);
-  zwlr_layer_surface_v1_set_anchor(layer_surface, anchor);
-  zwlr_layer_surface_v1_set_exclusive_zone(layer_surface, exclusive_zone);
-  zwlr_layer_surface_v1_set_margin(layer_surface, margin_top, margin_right, margin_bottom,
-                                   margin_left);
-  zwlr_layer_surface_v1_set_keyboard_interactivity(layer_surface, keyboard_interactive);
   zwlr_layer_surface_v1_add_listener(layer_surface, &layer_surface_listener, layer_surface);
   wl_surface_commit(wl_surface);
   wl_display_roundtrip(display);
 
-  egl_window = wl_egl_window_create(wl_surface, width, height);
-  assert(egl_window);
-  egl_surface = (struct wlr_egl_surface*) wlr_egl_create_surface(&egl, egl_window);
-  assert(egl_surface);
+  gtk_widget_show_all(window);
 
   wl_display_roundtrip(display);
-  draw();
+
+  gtk_main();
 
   while (wl_display_dispatch(display) != -1 && run_display) {
     // This space intentionally left blank
